9862cf8e631e547f7c1a455e00d9e5a6238d0707
will <denis.barbosa@fatec.sp.gov.br>
2022-02-20 20:01:43 -0300

    Query detalhes cliente fisica

--- adm_politeismo/src/main/java/br/com/rd/dao/DetalhesFisicaDao.java
+++ adm_politeismo/src/main/java/br/com/rd/dao/DetalhesFisicaDao.java
@@ -18,7 +18,29 @@ public class DetalhesFisicaDao {
 		DetalhesFisica dtFis = null;
 
 		try {
-			PreparedStatement ps = con.prepareStatement("");
+			PreparedStatement ps = con.prepareStatement("SELECT C.NOME, F.ID_CPF, T.DDD, T.CELULAR , E.TIPO_LOGRADOURO, E.NOME_LOGRADOURO, E.NUMERO, E.CEP, E.CIDADE, ES.SIGLA  \n"
+					+ "FROM\n"
+					+ "CLIENTE C\n"
+					+ "INNER JOIN\n"
+					+ "FISICA F \n"
+					+ "ON\n"
+					+ "F.CLIENTE_ID_CLIENTE  = C.ID_CLIENTE\n"
+					+ "INNER JOIN\n"
+					+ "TELEFONE T\n"
+					+ "ON\n"
+					+ "T.CLIENTE_ID_CLIENTE = C.ID_CLIENTE \n"
+					+ "INNER JOIN \n"
+					+ "CLIENTE_ENDERECO CE\n"
+					+ "ON\n"
+					+ "CE.CLIENTE_ID_ENDERECO = C.ID_CLIENTE\n"
+					+ "INNER JOIN \n"
+					+ "ENDERECO E\n"
+					+ "ON\n"
+					+ "CE.ENDERECO_ID_ENDERECO = E.ID_ENDERECO\n"
+					+ "INNER JOIN\n"
+					+ "ESTADO ES\n"
+					+ "ON\n"
+					+ "E.ESTADO_ID_ESTADO = ES.ID_ESTADO;");
 			ps.setInt(1, id);
 			ResultSet rs = ps.executeQuery();
 
@@ -28,14 +50,13 @@ public class DetalhesFisicaDao {
 				String ddd = ("DDD");
 				String celular = ("CELULAR");
 				String tipoLograd = ("TIPO_LOGRADOURO");
-				String logradouro = ("LOGRADOURO");
-				String numeracao = ("NUMERACAO");
+				String logradouro = ("NOME_LOGRADOURO");
+				String numeracao = ("NUMERO");
 				String cep = ("CEP");
 				String cidade = ("CIDADE");
-				String estado = ("ESTADO");
+				String estado = ("SIGLA");
 
-				dtFis = new DetalhesFisica(nome, cpf, ddd, celular, tipoLograd, logradouro, numeracao, cep, cidade,
-						estado);
+				dtFis = new DetalhesFisica(nome, cpf, ddd, celular, tipoLograd, logradouro, numeracao, cep, cidade,estado);
 				dtFis.setId(id);
 
 				ps.close();
