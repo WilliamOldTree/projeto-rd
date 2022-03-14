82efffd193f23abebef16bcc2326099cf5135d00
MurylloSoares <MurylloSoares>
2022-03-14 12:56:14 -0300

    arrumando conflitos

--- base-pi-react/src/routes.js
+++ base-pi-react/src/routes.js
@@ -1,19 +1,8 @@
 import { Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-import Home from './pages/home/Home'
-import Product from './pages/product/Product'
 import Cart from './pages/cart/Cart'
-<<<<<<< HEAD
 import { Empresa, ProcessoCompra, ProcessoEnvio, ProcessoGarantia, ProcessoPagamento, Seguranca } from "./pages/institucional";
-=======
-import { Empresa, ProcessoCompra } from "./pages/institucional";
-import Login from './pages/login/Login';
->>>>>>> 3f0c6e0caec2f1f47550221d241e70e225ee0d9e
-=======
 import Home from './pages/home/Home';
 import Product from './pages/product/Product';
-import Cart from './pages/cart/Cart';
-import { Empresa, ProcessoCompra, ProcessoEnvio, ProcessoGarantia, ProcessoPagamento, Seguranca } from "./pages/institucional";
 import Login from './pages/login/Login';
 import ClienteFisico from './pages/cadastro_ClienteFisico/Cadastro_ClienteFisico';
 import ClienteJuridico from './pages/cadastro_ClienteJuridico/Cadastro_ClienteJuridico';
@@ -27,22 +16,10 @@ import AreaEndereco from "./pages/area_cliente_enderecos/area_cliente_endereco";
 import AreaDados from "./pages/area_cliente_dados/area_cliente_dados";
 import AreaCartoes from "./pages/area_cliente_cartoes/area_cliente_cartoes";
 import AtendimentoCliente from "./pages/atendimento_ao_cliente/atendimento_ao_cliente";
-
->>>>>>> 3c7758da2ab06e880cf070d04f3b59c5d4d5f7e3
-
 export const Routes = () => {
     return (
         <Switch>
             <Route path="/" component={Home} exact/>
-<<<<<<< HEAD
-            <Route path="/institucional/empresa" component={Empresa}/>
-            <Route path="/institucional/processocompra" component={ProcessoCompra}/>
-            <Route path ="/institucional/processoenvio" component={ProcessoEnvio}/>
-            <Route path="/institucional/processogarantia" component={ProcessoGarantia}/>
-            <Route path="/institucional/processopagamento" component={ProcessoPagamento}/>
-            <Route path="/institucional/seguranca" component = {Seguranca}/>
-=======
->>>>>>> 3c7758da2ab06e880cf070d04f3b59c5d4d5f7e3
             <Route path="/product" component={Product} />
             <Route path="/cart" component={Cart} />
             <Route path="/login" component={Login} />
