09debf3b7550b47e082b8336296d6842ccb4c103
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-03-14 01:39:59 -0300

    Atualização Routs

--- base-pi-react/src/routes.js
+++ base-pi-react/src/routes.js
@@ -1,19 +1,26 @@
 import { Switch, Route } from "react-router-dom";
-import Home from './pages/home/Home'
-import Product from './pages/product/Product'
-import Cart from './pages/cart/Cart'
-import { Empresa, ProcessoCompra } from "./pages/institucional";
+import Home from './pages/home/Home';
+import Product from './pages/product/Product';
+import Cart from './pages/cart/Cart';
 import Login from './pages/login/Login';
+import ClienteFisico from './pages/cadastro_ClienteFisico/Cadastro_ClienteFisico';
+import ClienteJuridico from './pages/cadastro_ClienteJuridico/Cadastro_ClienteJuridico';
+import Cart_Success from './pages/cart_success/Cart_Success';
+import Favoritos from './pages/favorites/Area_cliente_favoritos';
+import Pedidos from './pages/requests/Area_cliente_pedidos';
 
 export const Routes = () => {
     return (
         <Switch>
             <Route path="/" component={Home} exact/>
-            <Route path="/institucional/empresa" component={Empresa}/>
-            <Route path="/institucional/processocompra" component={ProcessoCompra}/>
             <Route path="/product" component={Product} />
             <Route path="/cart" component={Cart} />
             <Route path="/login" component={Login} />
+            <Route path="/cadastro_ClienteFisico" component={ClienteFisico} />
+            <Route path="/cadastro_ClienteJuridico" component={ClienteJuridico} />
+            <Route path="/cart_success" component={Cart_Success} />
+            <Route path="/favorites/" component={Favoritos} />
+            <Route path="/requests" component={Pedidos} />
         </Switch>
     );
 }
