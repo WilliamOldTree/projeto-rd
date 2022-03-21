263834069f7f46ad962d3d1cefced6b1183d3722
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-03-21 11:15:31 -0300

    Atualização Menu

--- base-pi-react/src/components/menu/Menu.js
+++ base-pi-react/src/components/menu/Menu.js
@@ -5,25 +5,25 @@ function Menu() {
     return (
         <>
             <div className='menu-component'>
-                <Link className='menu-list-itens' to='area_cliente_dados'>Meus Dados
-                    <hr className='menu-lines'/>
+                <Link className='menu-component-itens' to='area_cliente_dados'>Meus Dados
+                    <hr className='menu-component-lines'/>
                 </Link>
-                <Link className='menu-list-itens'to='area_cliente_pedidos'>Meus Pedidos
-                    <hr className='menu-lines'/>
+                <Link className='menu-component-itens'to='area_cliente_pedidos'>Meus Pedidos
+                    <hr className='menu-component-lines'/>
                 </Link>
-                <Link className='menu-list-itens' to='area_cliente_cartoes'>Meus Cartões
-                    <hr className='menu-lines'/>
+                <Link className='menu-component-itens' to='area_cliente_cartoes'>Meus Cartões
+                    <hr className='menu-component-lines'/>
                 </Link>
-                <Link className='menu-list-itens' to='area_cliente_endereco'>Meus Endereços
-                    <hr className='menu-lines'/>
+                <Link className='menu-component-itens' to='area_cliente_endereco'>Meus Endereços
+                    <hr className='menu-component-lines'/>
                 </Link>
-                <Link className='menu-list-itens'  to='area_cliente_favoritos' >Meus Favoritos
-                    <hr className='menu-lines'/>
+                <Link className='menu-component-itens'  to='area_cliente_favoritos' >Meus Favoritos
+                    <hr className='menu-component-lines'/>
                 </Link>
-                <Link className='menu-list-itens' to='area_cliente_senha' >Alterar Senha
-                    <hr className='menu-lines'/>
+                <Link className='menu-component-itens' to='area_cliente_senha' >Alterar Senha
+                    <hr className='menu-component-lines'/>
                 </Link>
-                <Link className='menu-list-itens' to='/'>Sair
+                <Link className='menu-component-itens' href=''>Sair
                 </Link>
             </div>
         </>
