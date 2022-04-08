ef08698bd7e34564c49a1ecc828e3aefa8d5cd8e
Sergio <caue.sergio@gail.com>
2022-04-08 16:49:41 -0300

    Tabela Status Pedido sergio

---
+++ ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/repositories/StatusPedidoRepository.java
@@ -0,0 +1,16 @@
+package br.com.rd.politeismo.ecommerce.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rd.politeismo.ecommerce.orm.StatusPedido;
+
+@Repository
+public interface StatusPedidoRepository  extends JpaRepository<StatusPedido, Long>, CrudRepository<StatusPedido, Long>{
+	
+	public List<StatusPedido> findAll();
+
+}
