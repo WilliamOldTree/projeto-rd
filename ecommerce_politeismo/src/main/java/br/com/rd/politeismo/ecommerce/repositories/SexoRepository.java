4931e18177742371367447c7010df6c9bd769735
MurylloSoares <MurylloSoares>
2022-04-08 15:32:57 -0300

    atualizacoes muryllo 08/04

---
+++ ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/repositories/SexoRepository.java
@@ -0,0 +1,14 @@
+package br.com.rd.politeismo.ecommerce.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import br.com.rd.politeismo.ecommerce.orm.Sexo;
+
+@Repository
+public interface SexoRepository  extends JpaRepository<Sexo, Long>, CrudRepository<Sexo, Long>{
+	
+	public List<Sexo> findAll();
+
+}
