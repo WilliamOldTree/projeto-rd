6cd29b4a825a90e43d1c7a43bd2dda98e7ce30f9
will <denis.barbosa@fatec.sp.gov.br>
2022-04-10 13:23:03 -0300

    Favorito, tabela. classe e repository ok

--- ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/repositories/ItemNotaFiscalRepository.java
+++ ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/repositories/ItemNotaFiscalRepository.java
@@ -2,9 +2,11 @@ package br.com.rd.politeismo.ecommerce.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.rd.politeismo.ecommerce.orm.ItemNotaFiscal;
 
+@Repository
 public interface ItemNotaFiscalRepository extends JpaRepository<ItemNotaFiscal, Long> , CrudRepository<ItemNotaFiscal, Long>{
 
 }
