db30acc19d44ea98bfe55b6e2dc4c88a1fb675ce
will <denis.barbosa@fatec.sp.gov.br>
2022-04-24 15:05:42 -0300

    heranca cliente fisica e juridica ok

--- ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/orm/Cliente.java
+++ ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/orm/Cliente.java
@@ -1,28 +1,35 @@
 package br.com.rd.politeismo.ecommerce.orm;
 
-
-import java.util.List;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.MappedSuperclass;
 
-@Entity
-@Table(name="cliente")
+
+@MappedSuperclass
 public class Cliente {
-	
+
 	@Id
-	@Column(name="id_cliente")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(nullable=false)
 	private String nome;
-	@Column(nullable=false)
 	private String email;
+	private String username;
+	private String password;
+	
+	
+	public Cliente () {
+		
+	}
+	
+	public Cliente(Long id, String nome, String email, String username, String password) {
+		
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
 	
 	/*
 	 * @OneToMany(mappedBy="cliente") List <Telefone> telefone;
@@ -48,37 +55,36 @@ public class Cliente {
 	 * 
 	 * public List<Pix> getPix() { return pix; }
 	 */
-
-
-
-	public Cliente() {
-		
-	}
 	
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-}//end class
+}// end class
