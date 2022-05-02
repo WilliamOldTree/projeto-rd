1a886afd1df853fa8e1b01843588aade33c59c5b
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-05-02 17:55:51 -0300

    Alterações Projeto

---
+++ ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/entities/Cliente.java
@@ -0,0 +1,90 @@
+package br.com.qsd.politeismo.ecommerce.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.qsd.politeismo.ecommerce.enums.Genero;
+
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String nome;
+    private String email;
+    private String password;
+    private LocalDate nascimento;
+    
+	@Enumerated(EnumType.STRING)
+    private Genero genero;
+	
+	public Cliente() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDate getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(LocalDate nascimento) {
+		this.nascimento = nascimento;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+}
---
+++ ecommerce/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO endereco (ID_ENDERECO, apelido, NOME_LOGRADOURO, TIPO_LOGRADO, numero, cep, cidade, bairro) 
+VALUES(1, 'Casa', 'independencia', 'Rua' , '123','43700-000','Salvador', 'CIA 2');
+
