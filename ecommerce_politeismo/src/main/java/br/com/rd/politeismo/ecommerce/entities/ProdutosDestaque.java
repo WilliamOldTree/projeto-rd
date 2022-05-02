3a6deaff06a486f55e6e94228114567038c4a0ac
Fabiola Carvalho <facsantos@rd.com.br>
2022-05-02 11:32:54 -0300

    .

--- ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/entities/ProdutosDestaque.java
+++ ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/entities/ProdutosDestaque.java
@@ -35,21 +35,11 @@ public class ProdutosDestaque {
 	public void setDesconto(Double desconto) {
 		this.desconto = desconto;
 	}
-<<<<<<< HEAD:EcoormecePoliteismo/src/main/java/br/com/rd/queroserdev/loja/mvc/model/ProdutosDestaque.java
-<<<<<<< HEAD
+
 
 	@Override
 	public String toString() {
 		return "ProdutosDestaque [id_produto_destaque=" + id_produto_destaque + ", desconto=" + desconto + "]";
 	}
-	
-	
-}
-
-=======
 }
 	
->>>>>>> b021456381f5b7a75b450be99b2033625ca84e5a
-=======
-}
->>>>>>> 12ce479fdda516b02add838b91e153c6ca9c6944:ecommerce_politeismo/src/main/java/br/com/rd/politeismo/ecommerce/entities/ProdutosDestaque.java
