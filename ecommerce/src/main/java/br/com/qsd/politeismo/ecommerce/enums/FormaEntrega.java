f20f9bd15234774917867fd54229a35f5573dcd8
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-05-15 22:51:49 -0300

    Ajuste Entidades

--- ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/enums/FormaEntrega.java
+++
@@ -1,7 +0,0 @@
-package br.com.qsd.politeismo.ecommerce.enums;
-
-public enum FormaEntrega {
-	CORREIOS,
-	MOTO_FRETE,
-	APLICATIVOS;
-}
--- ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/enums/StatusEntrega.java
+++
@@ -1,7 +0,0 @@
-package br.com.qsd.politeismo.ecommerce.enums;
-
-public enum StatusEntrega {
-	ENTREGUE,
-	OBJETO_POSTADO,
-	DEVOLVIDO,
-}
