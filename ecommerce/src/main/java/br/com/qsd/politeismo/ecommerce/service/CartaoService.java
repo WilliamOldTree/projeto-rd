9c1866b6f10bd3c42bb30f6de6c532041697d93a
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-05-15 22:48:39 -0300

    ajuste

--- ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/service/CartaoService.java
+++ ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/service/CartaoService.java
@@ -58,10 +58,18 @@ public class CartaoService {
 			entity.setCliente(cliente.get());
 			
 			
-			
 			Optional<Forma> forma= frepository.findById(form.getCliente());
 			entity.setFormaPagamento(forma.get());		
+		
+
+		
+				
+			
+			
 			entity = repository.save(entity);
+			
+			
+
 			return new CartaoDTO(entity);
 
 		}
