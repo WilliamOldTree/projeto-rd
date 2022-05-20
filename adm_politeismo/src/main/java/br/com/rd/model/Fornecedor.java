c93915ae3b8fd6d40583e1b182fa0792cc5d6fd9
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-05-20 11:28:31 -0300

    Ajustes da DashBoard

--- adm_politeismo/src/main/java/br/com/rd/model/Fornecedor.java
+++ adm_politeismo/src/main/java/br/com/rd/model/Fornecedor.java
@@ -6,72 +6,54 @@ public class Fornecedor {
 	private String razao;
 	private String cnpj;
 	private String email;
-	private Boolean status_fornecedor;
-
-
-	public Fornecedor(String razao, String cnpj, String email) {
+	private String telefone;
+	private Boolean flInativo;
+	
+	
+	public Fornecedor(String razao, String cnpj, String email, String telefone) {
 		super();
 		this.razao = razao;
 		this.cnpj = cnpj;
 		this.email = email;
+		this.telefone = telefone;
 	}
-
-
+	
 	public Integer getId() {
 		return id;
 	}
-
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-
 	public String getRazao() {
 		return razao;
 	}
-
-
 	public void setRazao(String razao) {
 		this.razao = razao;
 	}
-
-
 	public String getCnpj() {
 		return cnpj;
 	}
-
-
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
-
-
 	public String getEmail() {
 		return email;
 	}
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
-	
-	public String toString() {
-		return "Fornecedor [id=" + id + ", razao=" + razao + ", cnpj=" + cnpj + ", email=" + email + "]";
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Boolean getFlInativo() {
+		return flInativo;
+	}
+	public void setFlInativo(Boolean flInativo) {
+		this.flInativo = flInativo;
 	}
 
-
-	public Boolean getStatus_fornecedor() {
-		return status_fornecedor;
-	}
-
-
-	public void setStatus_fornecedor(Boolean status_fornecedor) {
-		this.status_fornecedor = status_fornecedor;
-	}
-	
-	
 	
 }
