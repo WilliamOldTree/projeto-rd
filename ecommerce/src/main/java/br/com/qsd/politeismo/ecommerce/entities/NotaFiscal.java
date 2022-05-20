647b63eda594e92c6bc4d3079b1c4fb2f17e1d3b
Leia Borges Santos de Jesus <LBSJesus@rd.com.br>
2022-05-20 13:03:04 -0300

    Ajuste entidades

--- ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/entities/NotaFiscal.java
+++ ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/entities/NotaFiscal.java
@@ -1,12 +1,13 @@
 package br.com.qsd.politeismo.ecommerce.entities;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,8 +15,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import br.com.qsd.politeismo.ecommerce.enums.Operacao;
 
 
 @Entity
@@ -26,11 +27,20 @@ public class NotaFiscal {
 	@Id @Column(name = "id_nota_fiscal") 
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Long id;
+	
+	@Column(name = "codigo_acesso_chave") 
 	private Long codigoAcessoChave;
-	private Date dataNotaFiscal;
+	
+	@Column(name = "data_nota_fiscal") 
+	private LocalDate dataNotaFiscal;
+	
+	@Column(name = "numero_serie") 
 	private Long numeroSerie;
+	
 	private BigDecimal valor;
-	private String operacao;
+	
+	@Enumerated(EnumType.STRING)
+	private Operacao operacao;
 
 	@JsonIgnore @OneToMany(mappedBy="idNotaFiscal") 
 	private List<ItemNotaFiscal> itemNotaFiscal; 
@@ -41,41 +51,52 @@ public class NotaFiscal {
 	
 	public NotaFiscal() {
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Long getCodigoAcessoChave() {
 		return codigoAcessoChave;
 	}
+
 	public void setCodigoAcessoChave(Long codigoAcessoChave) {
 		this.codigoAcessoChave = codigoAcessoChave;
 	}
-	public Date getDataNotaFiscal() {
+
+	public LocalDate getDataNotaFiscal() {
 		return dataNotaFiscal;
 	}
-	public void setDataNotaFiscal(Date dataNotaFiscal) {
+
+	public void setDataNotaFiscal(LocalDate dataNotaFiscal) {
 		this.dataNotaFiscal = dataNotaFiscal;
 	}
+
 	public Long getNumeroSerie() {
 		return numeroSerie;
 	}
+
 	public void setNumeroSerie(Long numeroSerie) {
 		this.numeroSerie = numeroSerie;
 	}
+
 	public BigDecimal getValor() {
 		return valor;
 	}
+
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
-	public String getOperacao() {
+
+	public Operacao getOperacao() {
 		return operacao;
 	}
-	public void setOperacao(String operacao) {
+
+	public void setOperacao(Operacao operacao) {
 		this.operacao = operacao;
 	}
 
@@ -86,6 +107,14 @@ public class NotaFiscal {
 	public void setItemNotaFiscal(List<ItemNotaFiscal> itemNotaFiscal) {
 		this.itemNotaFiscal = itemNotaFiscal;
 	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
 	
-	
+
 }
