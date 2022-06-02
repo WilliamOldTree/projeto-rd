9b66b3bd37b33ec26eb213eeb189dc6170018134
will <denis.barbosa@fatec.sp.gov.br>
2022-06-02 16:55:25 -0300

    Developer atualizada will

--- ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/security/SecurityConfigurations.java
+++ ecommerce/src/main/java/br/com/qsd/politeismo/ecommerce/security/SecurityConfigurations.java
@@ -43,8 +43,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-<<<<<<< HEAD
-		
 		.antMatchers(HttpMethod.GET , "/produtos").permitAll()
 		.antMatchers(HttpMethod.GET , "/produtos/*").permitAll()
 		.antMatchers(HttpMethod.POST , "/produtos/**").permitAll()
@@ -52,25 +50,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET , "/departamentos/*").permitAll()
 		.antMatchers(HttpMethod.POST , "/clientes").permitAll()
 		.antMatchers(HttpMethod.POST , "/auth").permitAll()
-=======
-<<<<<<< HEAD
-//		.antMatchers(HttpMethod.GET , "/produtos").permitAll()
-//		.antMatchers(HttpMethod.GET , "/produtos/*").permitAll()
-//		.antMatchers(HttpMethod.GET , "/categorias").permitAll()
-//		.antMatchers(HttpMethod.GET , "/categorias/*").permitAll()
-//		.antMatchers(HttpMethod.POST , "/clientes").permitAll()
-//		.antMatchers(HttpMethod.POST , "/auth").permitAll()
-//		.antMatchers(HttpMethod.GET , "/pedidos/*").permitAll()
-//		.antMatchers(HttpMethod.GET , "/pedidos").permitAll()
-//		.antMatchers(HttpMethod.POST , "/pedidos").permitAll()
-//		.antMatchers(HttpMethod.GET , "/itensPedido/*").permitAll()
-//		.antMatchers(HttpMethod.GET , "/itensPedido").permitAll()
-//		.antMatchers(HttpMethod.POST , "/itensPedido").permitAll()
-		
-		
-		.antMatchers(HttpMethod.GET , "/*").permitAll()
-		.antMatchers(HttpMethod.POST , "/*").permitAll()
->>>>>>> 86e1c0a1c78de88577ad12078da4115158ec772a
 		.antMatchers(HttpMethod.GET , "/pedidos/*").permitAll()
 		.antMatchers(HttpMethod.GET , "/pedidos").permitAll()
 		.antMatchers(HttpMethod.POST , "/pedidos/*/*").permitAll()
@@ -80,27 +59,14 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET , "/itensPedido/*/*").permitAll()
 		.antMatchers(HttpMethod.GET , "/itensPedido").permitAll()
 		.antMatchers(HttpMethod.POST , "/itensPedido/*").permitAll()
-<<<<<<< HEAD
-
-
-
-=======
-		
-		
-=======
-		.antMatchers(HttpMethod.GET , "/produtos").permitAll()
-		.antMatchers(HttpMethod.GET , "/produtos/*").permitAll()
-		.antMatchers(HttpMethod.POST , "/produtos/**").permitAll()
-		.antMatchers(HttpMethod.GET , "/departamentos").permitAll()
-		.antMatchers(HttpMethod.GET , "/departamentos/*").permitAll()
-		.antMatchers(HttpMethod.POST , "/clientes").permitAll()
-		.antMatchers(HttpMethod.POST , "/auth").permitAll()
->>>>>>> 955bc3b69e1378e89cea6b984af3ec473700e377
->>>>>>> 86e1c0a1c78de88577ad12078da4115158ec772a
 		.anyRequest().authenticated().and().cors()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService , clienteRepository) , UsernamePasswordAuthenticationFilter.class);
+		
+
+
+
 	}
 	
 	@Override
